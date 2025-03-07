a0747a6336c40b24967094e7b400073a85b24b56
--replace-all <stev508ga@gmail.com>
2025-03-07 10:59:47 -0500

    if simple

---
+++ if_simple.py
@@ -0,0 +1,13 @@
+'''
+sintaxis if simple:
+if condicional :
+    instruccion1
+    instruccion2  
+'''
+#ejemplo
+a=9
+b=3
+if a > b :
+
+    print("a es mayor que b")
+    print("y hoy es viernes")
